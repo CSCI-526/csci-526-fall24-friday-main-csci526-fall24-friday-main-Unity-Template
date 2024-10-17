969fccf8c245cc6a76e1f56d2edbb78bc804609d
AngeliaZddl <78582332+AngeliaZddl@users.noreply.github.com>
2024-10-17 00:08:26 -0700

    update

--- Alpha/Assets/Scripts/MainMenu.cs
+++ Alpha/Assets/Scripts/MainMenu.cs
@@ -8,12 +8,14 @@ public class MainMenuController : MonoBehaviour
     public string gameScene = "SampleScene";
     public void StartGame()
     {
+        Debug.Log("start");
         ReturnToGame();  // 加载游戏的主场景
     }
 
     // 退出游戏
     public void QuitGame()
     {
+        Debug.Log("quit");
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;  // 如果在编辑器中，停止播放
         #else
